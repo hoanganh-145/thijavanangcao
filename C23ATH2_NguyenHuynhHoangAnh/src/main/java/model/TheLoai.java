1d65776af5acbbb660e703e9b1ae67ee6f0f1729
ADMIN <ADMIN@PC232>
2024-11-12 15:18:02 +0700

    KT1

---
+++ C23ATH2_NguyenHuynhHoangAnh/src/main/java/model/TheLoai.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class TheLoai {
+    private int macd;
+    private String tencd;
+
+    public TheLoai() {
+    }
+
+    public TheLoai(int macd) {
+        this.macd = macd;
+    }
+
+    public TheLoai(int macd, String tencd) {
+        this.macd = macd;
+        this.tencd = tencd;
+    }
+
+    public int getMacd() {
+        return macd;
+    }
+
+    public void setMacd(int macd) {
+        this.macd = macd;
+    }
+
+    public String getTencd() {
+        return tencd;
+    }
+
+    public void setTencd(String tencd) {
+        this.tencd = tencd;
+    }
+
+    @Override
+    public String toString() {
+        return "ChuDe{" + "macd=" + macd + ", tencd=" + tencd + '}';
+    }    
+}
